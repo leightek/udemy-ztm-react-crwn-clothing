3887db9809fefb66a84d290a5d46a8dae234287e
Leigh Su <slhcan@yahoo.com>
2023-07-10 00:09:53 -0700

    [ReactRetail] Moved categories to different folder with imageUrl data added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import CateogoryItem from './components/category-item/category-item.component';
-
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
       title: 'Jacket',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
       title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
       title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
     },
   ];
-  return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <CateogoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+
+  return <Directory categories={categories} />;
 };
 
 export default App;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CateogoryItem from '../category-item/category-item.component';
+
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+    return (
+        <div className='categories-container'>
+          {categories.map((category) => (
+            <CateogoryItem key={category.id} category={category} />
+          ))}
+        </div>
+    );
+};
+
+export default Directory;
