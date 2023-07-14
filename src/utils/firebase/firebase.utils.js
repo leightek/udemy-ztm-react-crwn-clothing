867fe2e5994ef0bdcda307142221307774e3aec0
Leigh Su <slhcan@yahoo.com>
2023-07-13 23:49:59 -0700

    [ReactRetail] Added firebase setup and sign in component

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSPodNFhhQfysKQBWJS32CzhwuQkDCQEs",
+    authDomain: "crwn-clothing-db-3b108.firebaseapp.com",
+    projectId: "crwn-clothing-db-3b108",
+    storageBucket: "crwn-clothing-db-3b108.appspot.com",
+    messagingSenderId: "718081917254",
+    appId: "1:718081917254:web:92db10ddbb02ed6799a547"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
