adb7c639beba02d052d05d0d4ed194435f12ffbd
Leigh Su <slhcan@yahoo.com>
2023-07-14 23:43:35 -0700

    [ReactRetail] Added authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,15 @@
+import { createUserProfileDocument, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        createUserProfileDocument(response);
+    };
+
     return (
         <div>
             <h1>Sign In Page</h1>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
         </div>
     );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDSPodNFhhQfysKQBWJS32CzhwuQkDCQEs",
@@ -9,4 +10,19 @@ const firebaseConfig = {
     appId: "1:718081917254:web:92db10ddbb02ed6799a547"
   };
 
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  console.log(userAuth);
+};
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
